ec482f1acaad88929ee2d58f4a9d5bdb9abfd2c6
Joowan Kim <rlawndhks217@gmail.com>
2021-04-10 14:44:27 +0900

    add indentation at BaseTest.py

--- tests/baseTest.py
+++ tests/baseTest.py
@@ -16,14 +16,14 @@ class BaseTest(TestCase):
 
     def setUp(self):
         self.latte = Coffee(id=self.LATTE_ID,
-                       name='latte',
-                       price=4000)
+                            name='latte',
+                            price=4000)
         self.espresso = Coffee(id=self.ESPRESSO_ID,
-                          name='espresso',
-                          price=2000)
+                               name='espresso',
+                               price=2000)
         self.americano = Coffee(id=self.AMERICANO_ID,
-                           name='americano',
-                           price=500)
+                                name='americano',
+                                price=500)
 
         self.menu = Menu()
         self.coffee_list = [self.latte, self.espresso, self.americano]
